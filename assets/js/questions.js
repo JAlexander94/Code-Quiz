24004db3182d9c1ce86a5d70ec412a268f1979ce
JAlexander94 <118635922+JAlexander94@users.noreply.github.com>
2023-01-24 00:47:31 +0000

    highscores saved to local storage

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,6 +1,6 @@
 //define the question and answers in arrays so that can be used with a for loop to append them to the HTML
 let questionsarr = ["Commonly used data types do not include:","The condition in an IF/ELSE statement is enclosed within _","Arrays in javascript can be used to store _","String values must be enclosed within _ when being assigned to variables.","A very useful tool used during development and debugging for printing content to the debugger is"]
-let answersarr = ["alerts","parentheses","all of the above","quotation marks","console log"]
+let answersarr = ["3. alerts","3. parentheses","4. all of the above","3. quotation marks","4. console log"]
 let allanswersarr = [["strings","booleans","alerts","numbers"],["quotes","curly brackets","parentheses","square brackets"],["numbers and strings","other arrays","booleans","all of the above"],["commas","curly brackets","quotation marks","parentheses"],["javascript","terminal/bash","for loops","console log"]]
 
 let startscreen = document.getElementById("start-screen")
@@ -10,8 +10,10 @@ let questiontitle = document.getElementById("question-title")
 let choices = document.getElementById("choices")
 let finalscore = document.getElementById("final-score")
 let time = document.getElementById("time")
+let initials
 
 var numquestions = 0
+var highscores = []
 
 function firstquestion(){
     startscreen.setAttribute("class","hide")
@@ -28,25 +30,29 @@ function firstquestion(){
     }
     choices.appendChild(questionsshell)
     numquestions++
-    questions.addEventListener("click",function(event){
-        const isButton = event.target.nodeName === "BUTTON"
-        if(!isButton){return}
-        const answer = event.target.textContent
-        if(answer!==answersarr[(numquestions-1)]){secondsLeft=secondsLeft-10}
-            if(numquestions === 5 ){
-                finalscore.textContent = secondsLeft
-                questions.setAttribute("class","hide")
-                endscreen.setAttribute("class","visible")
-            }else{        
-                questiontitle.textContent = questionsarr[numquestions]
-                for (i=0;i<4;i++){
-                const button = document.getElementById(i)
-                button.textContent = (i+1)+". "+allanswersarr[numquestions][i]
-                }
-            }
-        numquestions++
-})
+    questions.addEventListener("click",function(event){newquestion(event)})
 }
+
+
+function newquestion(event){
+    const isButton = event.target.nodeName === "BUTTON"
+    if(!isButton){return}
+    const answer = event.target.textContent
+    if(answer!==answersarr[(numquestions-1)]){secondsLeft=secondsLeft-10}
+        if(numquestions === 5 ){
+            finalscore.textContent = secondsLeft
+            questions.setAttribute("class","hide")
+            endscreen.setAttribute("class","visible")
+        }else{        
+            questiontitle.textContent = questionsarr[numquestions]
+            for (i=0;i<4;i++){
+            const button = document.getElementById(i)
+            button.textContent = (i+1)+". "+allanswersarr[numquestions][i]
+            }
+        }
+    numquestions++
+}
+
 
 function setTime(){
     var timerInterval = setInterval(function(){
@@ -60,4 +66,19 @@ function setTime(){
     },1000)
 }
 
+
+function highscore(){
+    const storedhighscores = JSON.parse(localStorage.getItem("highscores"))
+    if(storedhighscores !== null){highscores=storedhighscores}
+    const highscore = finalscore.textContent
+    const initials = document.getElementById("initials").value
+    const newhighscore = [initials,highscore]
+    highscores.push(newhighscore)
+    console.log(highscores)
+    localStorage.setItem("highscores",JSON.stringify(highscores))
+}
+
+
 document.getElementById("start").onclick = function(){firstquestion()}
+
+document.getElementById("submit").onclick = function(){highscore()}
